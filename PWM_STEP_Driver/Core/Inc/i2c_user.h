4c2e8bdf55033c5ea5f82364bf3779ecca67aa16
Chipizdry <79665980+Chipizdry@users.noreply.github.com>
2023-08-09 12:24:14 +0300

    09.08.2023_Sch_update

---
+++ PWM_STEP_Driver/Core/Inc/i2c_user.h
@@ -0,0 +1,15 @@
+/*
+ * i2c_user.h
+ *
+ *  Created on: Jul 24, 2023
+ *      Author: chipi
+ */
+
+#ifndef INC_I2C_USER_H_
+#define INC_I2C_USER_H_
+
+#include "stm32f3xx.h"
+//------------------------------------------------
+
+
+#endif /* INC_I2C_USER_H_ */
